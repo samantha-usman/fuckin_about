cac30e28d73c8de71e7025f507b461ca0fc72fb1
samantha-usman <sausma01@syr.edu>
2019-05-16 15:14:05 -0500

    deleting extraneous files

--- random_shit.f90
+++
@@ -1,8 +0,0 @@
-program fortrantutorial			! starts the program
-	implicit none 			! clears all variables
-	character*20 :: name		! create a variable (character array) named "name"
-	print *, "What's your name?"	! printing question to screen
-	read *, name			! reading in input, storing it under "name"
-	print *, "Hello, ", name	! printing response to screen
-
-end program fortrantutorial		! ends the program
